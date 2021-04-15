94fc3e110db68568cd63797c7ec3547431ca0fba
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-04-15 15:26:31 -0700

    Added badges and licensing info

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![${license} license](${renderLicenseLink(license)})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `https://img.shields.io/badge/license-${license}-blue`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseInfo;
+  switch (license) {
+    case 'Apache2':
+      licenseInfo = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+      
+      break;
+    case 'BSD-3_Clause':
+      licenseInfo = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+   
+      break;
+    case 'MIT':
+      licenseInfo = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+      
+      break;
+    case 'Mozilla Public License2.0' :
+    licenseInfo = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+    default:
+      licenseInfo = '';
+      break;
+
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
