dc951f3c94c9384e5a7eb517cdddb5fa0aeb5b3b
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-04-15 18:22:09 -0700

    Successful code test

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# dynamic_readME
+![License](https://img.shields.io/badge/license-undefined-blue);
+
+# c  
+
+## Table of Contents
+- [Description](#Description)
+- [Installation](#Install-Instructions)
+- [Usage](#Usage)
+- [Contribution](#Contribution-Guidelines)
+- [Testing](#Test-Instructions)
+- [License](#License)
+- [Questions](#Questions?-Contact-Me!)
+
+## Description
+
+d  
+  
+## Install Instructions  
+e  
+
+## Usage  
+e  
+
+## Licenses
+## License 
+ ${licenseText}
+
+## Contribution Guidelines  
+a  
+  
+## Test Instructions  
+d  
+
+
+## Questions? 
+e
+
+Get in touch!
+Find me on [Github!](https://github.com/undefined)
--- index.js
+++ index.js
@@ -1,87 +1,129 @@
-const inquirer = require("inquirer");
-const fs = require('fs');
-const generateMarkdown = require ('./generateMarkdown');
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./generateMarkdown");
+const renderLicenseSection = require('./generateMarkdown');
 
-// TODO: Include packages needed for this application
-function input() {
-    return inquirer.prompt()[
-        {
-            type: "input",
-            name: "todo",
-            message:v
-        }
-    ]
+
+const inquirer = require("inquirer");
+
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const userQuestions = async () => {
+  let data = await inquirer.prompt([
+    {
+      type: "input",
+      name: "projectName",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "projDescription",
+      message: "Tell me what your project is about:",
+    },
+    {
+        type: "input",
+        name: "useInstructions",
+        message: "Tell me how to install your project:",
+      },
+
+    {
+      type: "input",
+      name: "howTo",
+      message: "Tell me how to use your project:",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What is your licensing information?",
+      choices: [
+        "Apache2",
+        "BSD-3-Clause",
+        "MIT license",
+        "Mozilla Public License2.0",
+      ],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Who contributed to your project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "How do we test it?",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "What is your github?",
+    },
+  ]).then(data => {
+      return data;
+  });
+  return data;
 };
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'Name',
-        message: 'What is your name?'
-    },
-    {
-        type: 'input',
-        name: 'ProjectName',
-        message:'What is the name of your project?'
-    },
-    {
-        type: 'input',
-        name: 'Description',
-        message: 'Tell me what your project is about:'
-    },
-    {
-        type: 'input',
-        name: 'Instructions',
-        message: 'Tell me how to use your project:'
-    },
-    {
-        type: 'list',
-        name: 'License',
-        message: 'What is your licensing information?',
-        choices: ['Apache2','BSD-3-Clause','GNU General Public License (GPL)',
-            'GNU Lesser General Public License (LGPL)'
-            ,'MIT license'
-            ,'Mozilla Public License2.0']
-    },
-    {
-        type: 'input',
-        name: 'Contributing',
-        message:'Who contributed to your project?'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'How do we test it?'
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'If you have any questions you can reach me at'
-    },
+
+const generateReadme = (answers) => {
+let markdownString = `![License](https://img.shields.io/badge/license-${answers.licenses}-blue);
+
+# ${answers.projectName}  
+
+## Table of Contents
+- [Description](#Description)
+- [Installation](#Install-Instructions)
+- [Usage](#Usage)
+- [Contribution](#Contribution-Guidelines)
+- [Testing](#Test-Instructions)
+- [License](#License)
+- [Questions](#Questions?-Contact-Me!)
+
+## Description
+
+${answers.projDescription}  
   
-];
+## Install Instructions  
+${answers.useInstructions}  
+
+## Usage  
+${answers.howTo}  
+
+## Licenses
+${renderLicenseSection(answers.license)}
+
+## Contribution Guidelines  
+${answers.contributing}  
+  
+## Test Instructions  
+${answers.tests}  
+
+
+## Questions? 
+${answers.questions}
+
+Get in touch!
+Find me on [Github!](https://github.com/${answers.github})`
+
+return markdownString;
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+const init = async () => {
+  try {
+    const answers = await userQuestions() 
+    console.log(answers);
+     fs.writeFile (`README.md`,
+      generateReadme(answers),
+      (err) => {
+          console.log('it worked');
+      }
+    );
+      console.log("README Created!");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Function call to initialize app
 init();
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
